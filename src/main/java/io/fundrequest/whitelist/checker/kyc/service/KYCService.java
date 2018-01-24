a154656a81b3e1fcc57947f3cc24c7cb9de27605
Quinten De Swaef <quintendeswaef@gmail.com>
2018-01-24 15:46:24 +0100

    batch insert

--- src/main/java/io/fundrequest/whitelist/checker/kyc/service/KYCService.java
+++ src/main/java/io/fundrequest/whitelist/checker/kyc/service/KYCService.java
@@ -1,6 +1,9 @@
 package io.fundrequest.whitelist.checker.kyc.service;
 
+import io.fundrequest.whitelist.checker.kyc.domain.KYCEntry;
 import io.fundrequest.whitelist.checker.kyc.dto.KYCResultDto;
+import io.fundrequest.whitelist.checker.kyc.repository.KYCRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,9 +11,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class KYCService {
 
+    @Autowired
+    private KYCRepository kycRepository;
+
     @Transactional(readOnly = true)
     public KYCResultDto search(final String address) {
 
+    }
+
+    @Transactional
+    public void bulk(Iterable<KYCEntry> kycEntries) {
+        kycRepository.deleteAll();
+        kycRepository.save(kycEntries);
     }
 
 

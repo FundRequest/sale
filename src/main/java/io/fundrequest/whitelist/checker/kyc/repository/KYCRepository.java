d99c24b7f131a24a1bc819de3869f91920b45342
Quinten De Swaef <quintendeswaef@gmail.com>
2018-01-25 12:50:03 +0100

    case insensitive

--- src/main/java/io/fundrequest/whitelist/checker/kyc/repository/KYCRepository.java
+++ src/main/java/io/fundrequest/whitelist/checker/kyc/repository/KYCRepository.java
@@ -2,13 +2,16 @@ package io.fundrequest.whitelist.checker.kyc.repository;
 
 import io.fundrequest.whitelist.checker.infrastructure.JpaRepository;
 import io.fundrequest.whitelist.checker.kyc.domain.KYCEntry;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface KYCRepository extends JpaRepository<KYCEntry, Long> {
 
+    @Query("select count(kycEntry) from KYCEntry kycEntry where lower(referredBy) like lower(:address)")
     Long countAllByReferredBy(@Param("referredBy") final String referralKey);
 
+    @Query("select kycEntry from KYCEntry kycEntry where lower(address) like lower(:address)")
     Optional<KYCEntry> findByAddress(@Param("address") final String address);
 }

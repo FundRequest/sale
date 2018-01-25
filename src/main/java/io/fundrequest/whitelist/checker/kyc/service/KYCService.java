bdfb6143a5e6e2071865f5cffa6a27d8751e53c9
Quinten De Swaef <quintendeswaef@gmail.com>
2018-01-25 13:53:28 +0100

    added address check

--- src/main/java/io/fundrequest/whitelist/checker/kyc/service/KYCService.java
+++ src/main/java/io/fundrequest/whitelist/checker/kyc/service/KYCService.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 
 @Service
 public class KYCService {
+
+    private static final Pattern ignoreCaseAddrPattern = Pattern.compile("(?i)^(0x)?[0-9a-f]{40}$");
 
     @Autowired
     private KYCRepository kycRepository;
@@ -19,15 +22,24 @@ public class KYCService {
     @Transactional(readOnly = true)
     public Optional<KYCResultDto> search(final String address) {
         final Optional<KYCEntry> byAddress = kycRepository.findByAddress(address);
-        return byAddress
-                .map(entry -> new KYCResultDto()
-                        .setAddress(entry.getAddress())
-                        .setStatus(entry.getStatus())
-                        .setMessage(entry.getMessage()))
-                .map(x -> x.setReferralCount(
-                        kycRepository.countAllByReferredBy(x.getAddress()
-                        ))
-                );
+
+        if (isValidAddress(address)) {
+            return byAddress
+                    .map(entry -> new KYCResultDto()
+                            .setAddress(entry.getAddress())
+                            .setStatus(entry.getStatus())
+                            .setMessage(entry.getMessage()))
+                    .map(x -> x.setReferralCount(
+                            kycRepository.countAllByReferredBy(x.getAddress()
+                            ))
+                    );
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private boolean isValidAddress(final String address) {
+        return ignoreCaseAddrPattern.matcher(address).find();
     }
 
     @Transactional

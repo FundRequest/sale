4666a656a83b08de0563381a53b5a99f2254ed41
davyvanroy <davy.van.roy@gmail.com>
2018-02-01 17:43:53 +0100

    add sale details

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 (function () {
-
+ var x = 3;
   jQuery(document).ready(function () {
+    $('#clock').countdown({
+      date: new Date(2018, 1, 12, 10, 0, 0),
+      offset: 1
+    }, function () {
+
+    });
   });
 
 
